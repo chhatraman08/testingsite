6916f22a87b903a222c0fd81097faeeba220cc4b
chhatraman08 <chhatramanshrestha@outlook.com>
2017-10-11 07:32:44 +0545

    just learning

---
+++ test/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+
+
+[assembly: OwinStartup(typeof(test.Startup))]
+
+namespace test
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+
+            app.MapSignalR();
+
+        }
+    }
+}
---
+++ test/chat.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace test
+{
+    public class chat : Hub
+    {
+        static string s1="";
+        public void onconn()
+        {
+
+        }
+        public void Hello(string msg)
+        {
+            Clients.Others.hello(msg);
+        }
+
+        
+        public void sendmsg(string s,string username)
+        {
+            s1 += "<div class='receivemsg'>" + s + "<span class='receiver'>" + username + "</span></div>";
+            Clients.Others.receivemsg(s,username);
+        }
+
+        public void getall()
+        {
+            Clients.Caller.loadall(s1);
+        }
+    }
+}
